5bb9083b8db873be385b29da2545f18b686c78f2
achester88 <anthonychester71@gmail.com>
2024-07-20 16:08:22 -0400

    Added splashscreen and work on config.json initialization

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { invoke } from '@tauri-apps/api/tauri'
+
 
 import Greet from './greet'
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use std::fs;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+          let splashscreen_window = app.get_window("splashscreen").unwrap();
+          let main_window = app.get_window("main").unwrap();
+          // we perform the initialization code on a new task so the app doesn't freeze
+          let dir = app.path_resolver().app_data_dir().expect("couldn't resolve app data dir");
+          tauri::async_runtime::spawn(async move {
+            // initialize your app here instead of sleeping :)
+            println!("Initializing...");
+            std::thread::sleep(std::time::Duration::from_secs(2));
+            println!("{:?}", dir);
+            match fs::read_to_string(dir.join("/config.json")) {
+                Ok(val) => {
+                   println!("config: {:?}", val);
+                },
+                Err(_) => {
+                    match fs::write(dir.join("/config.json"), b"Lorem ipsum") {
+                        Err(mes) => println!("ERROR: {:?}", mes),
+                        _ => {}
+                        }
+                }
+            }
+            println!("Done initializing.");
+
+            // After it's done, close the splashscreen and display the main window
+            splashscreen_window.close().unwrap();
+            main_window.show().unwrap();
+          });
+          Ok(())
+     })
     .plugin(tauri_plugin_sql::Builder::default().build())
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
